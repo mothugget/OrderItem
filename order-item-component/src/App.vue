ce80db81e57c6deeb6b4490b3013f5c4f79e54de
Karl Fredriksson <karlpafredriksson@gmail.com>
2023-06-24 11:06:44 +0200

    hard reset on linter issues

---
+++ order-item-component/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <OrderItem/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import OrderItem from './components/OrderItem.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    OrderItem
+  }
+})
+</script>
---
+++ order-item-component/src/components/OrderItem.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>{{PRODUCT_LIST[0]}}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    type Address = {
+      name: string;
+      company: string;
+      street: string;
+      city: string;
+      postalCode: string;
+      country: string;
+    };
+
+    type Product = {
+      sku: string;
+      title: string;
+      category: string;
+      price: number;
+    };
+
+    type OrderItem = {
+      orderRef: string;
+      address: Address;
+      productList: Product[];
+    };
+
+    console.log('hello');
+
+    const PRODUCT_LIST: Product[] = [
+      {
+        sku: '86ZL82ERVQ',
+        title: 'Washer',
+        category: 'Industry',
+        price: 38.09,
+      },
+      {
+        sku: '9LU38YOJ8A',
+        title: 'Piston',
+        category: 'Industry',
+        price: 81.14,
+      },
+      {
+        sku: 'LJ1AUIWQZ6',
+        title: 'Cam',
+        category: 'Automotive',
+        price: 40.34,
+      },
+      {
+        sku: '6EFD0C00DI',
+        title: 'Clamp',
+        category: 'Nautical',
+        price: 68.62,
+      },
+      {
+        sku: 'ZJY1JYB1A1',
+        title: 'Bolt',
+        category: 'Automotive',
+        price: 73.52,
+      },
+      {
+        sku: 'KDPRURUR2M',
+        title: 'Valve',
+        category: 'Industry',
+        price: 69.95,
+      },
+      {
+        sku: 'EIQ25ZB9UQ',
+        title: 'Nut',
+        category: 'Automotive',
+        price: 75.36,
+      },
+      {
+        sku: 'CMVI6FA5BC',
+        title: 'Bearing',
+        category: 'Industry',
+        price: 38.87,
+      },
+    ];
+
+    const ORDER_LIST = ref<OrderItem[]>([
+      {
+        orderRef: 'GNV0F',
+        address: {
+          name: 'Michael Johnson',
+          company: '123 Industries',
+          street: '789 Oak Avenue',
+          city: 'Sydney',
+          postalCode: '2000',
+          country: 'Australia',
+        },
+        productList: [
+          {
+            sku: 'EIQ25ZB9UQ',
+            title: 'Nut',
+            category: 'Automotive',
+            price: 75.36,
+          },
+        ],
+      },
+      {
+        orderRef: '6BFQ0',
+        address: {
+          name: 'Jane Smith',
+          company: 'XYZ Corp',
+          street: '456 Elm Street',
+          city: 'London',
+          postalCode: 'SW1A 1AA',
+          country: 'United Kingdom',
+        },
+        productList: [
+          {
+            sku: 'KDPRURUR2M',
+            title: 'Valve',
+            category: 'Industry',
+            price: 69.95,
+          },
+          {
+            sku: 'LJ1AUIWQZ6',
+            title: 'Cam',
+            category: 'Automotive',
+            price: 40.34,
+          },
+          {
+            sku: '86ZL82ERVQ',
+            title: 'Washer',
+            category: 'Industry',
+            price: 38.09,
+          },
+        ],
+      },
+      {
+        orderRef: 'SCFK0',
+        address: {
+          name: 'John Doe',
+          company: 'ABC Company',
+          street: '123 Main Street',
+          city: 'New York',
+          postalCode: '10001',
+          country: 'United States',
+        },
+        productList: [
+          {
+            sku: '9LU38YOJ8A',
+            title: 'Piston',
+            category: 'Industry',
+            price: 81.14,
+          },
+        ],
+      },
+    ]);
+
+    return { PRODUCT_LIST, ORDER_LIST };
+  },
+});
+</script>
