c3d8498c5c791e1f0927e5d52ef8e027953e2f7a
Karl Fredriksson <karlpafredriksson@gmail.com>
2023-06-26 13:08:45 +0200

    mock up to date

--- order-item-component/src/components/OrderList.vue
+++ order-item-component/src/components/OrderList.vue
@@ -83,127 +83,16 @@ export default defineComponent({
 
     const ORDER_LIST = ref<OrderItemType[]>([
   {
-    orderRef: 'Wh5R0',
+    orderRef: 'MR1Zq',
     address: {
       name: 'John Doe',
       company: 'ABC Company',
       street: '123 Main Street',
-      city: 'Example City',
+      city: 'City1',
       postalCode: '12345',
-      country: 'Country'
+      country: 'Country1'
     },
     productList: [
-      {
-        product: {
-          sku: 'LJ1AUIWQZ6',
-          title: 'Cam',
-          category: 'Automotive',
-          price: 40.34
-        },
-        quantity: 1
-      },
-      {
-        product: {
-          sku: '86ZL82ERVQ',
-          title: 'Washer',
-          category: 'Industry',
-          price: 38.09
-        },
-        quantity: 1
-      }
-    ]
-  },
-  {
-    orderRef: 'vA3Gw',
-    address: {
-      name: 'John Doe',
-      company: 'ABC Company',
-      street: '123 Main Street',
-      city: 'Example City',
-      postalCode: '12345',
-      country: 'Country'
-    },
-    productList: [
-      {
-        product: {
-          sku: 'LJ1AUIWQZ6',
-          title: 'Cam',
-          category: 'Automotive',
-          price: 40.34
-        },
-        quantity: 4
-      },
-      {
-        product: {
-          sku: 'EIQ25ZB9UQ',
-          title: 'Nut',
-          category: 'Automotive',
-          price: 75.36
-        },
-        quantity: 2
-      },
-      {
-        product: {
-          sku: 'LJ1AUIWQZ6',
-          title: 'Cam',
-          category: 'Automotive',
-          price: 40.34
-        },
-        quantity: 2
-      }
-    ]
-  },
-  {
-    orderRef: 'G4Zx2',
-    address: {
-      name: 'John Doe',
-      company: 'ABC Company',
-      street: '123 Main Street',
-      city: 'Example City',
-      postalCode: '12345',
-      country: 'Country'
-    },
-    productList: [
-      {
-        product: {
-          sku: 'KDPRURUR2M',
-          title: 'Valve',
-          category: 'Industry',
-          price: 69.95
-        },
-        quantity: 5
-      }
-    ]
-  },
-  {
-    orderRef: 'tBMMb',
-    address: {
-      name: 'John Doe',
-      company: 'ABC Company',
-      street: '123 Main Street',
-      city: 'Example City',
-      postalCode: '12345',
-      country: 'Country'
-    },
-    productList: [
-      {
-        product: {
-          sku: 'EIQ25ZB9UQ',
-          title: 'Nut',
-          category: 'Automotive',
-          price: 75.36
-        },
-        quantity: 2
-      },
-      {
-        product: {
-          sku: 'CMVI6FA5BC',
-          title: 'Bearing',
-          category: 'Industry',
-          price: 38.87
-        },
-        quantity: 4
-      },
       {
         product: {
           sku: 'ZJY1JYB1A1',
@@ -211,19 +100,121 @@ export default defineComponent({
           category: 'Automotive',
           price: 73.52
         },
+        quantity: 1
+      }
+    ]
+  },
+  {
+    orderRef: 'wt3l5',
+    address: {
+      name: 'Jane Smith',
+      company: 'XYZ Corporation',
+      street: '456 Elm Street',
+      city: 'City2',
+      postalCode: '67890',
+      country: 'Country2'
+    },
+    productList: [
+      {
+        product: {
+          sku: 'ZJY1JYB1A1',
+          title: 'Bolt',
+          category: 'Automotive',
+          price: 73.52
+        },
+        quantity: 5
+      },
+      {
+        product: {
+          sku: '86ZL82ERVQ',
+          title: 'Washer',
+          category: 'Industry',
+          price: 38.09
+        },
+        quantity: 2
+      }
+    ]
+  },
+  {
+    orderRef: 'gpn2n',
+    address: {
+      name: 'John Doe',
+      company: 'ABC Company',
+      street: '123 Main Street',
+      city: 'City1',
+      postalCode: '12345',
+      country: 'Country1'
+    },
+    productList: [
+      {
+        product: {
+          sku: 'ZJY1JYB1A1',
+          title: 'Bolt',
+          category: 'Automotive',
+          price: 73.52
+        },
+        quantity: 2
+      },
+      {
+        product: {
+          sku: '9LU38YOJ8A',
+          title: 'Piston',
+          category: 'Industry',
+          price: 81.14
+        },
         quantity: 3
       }
     ]
   },
   {
-    orderRef: '51mTk',
+    orderRef: 'i94Dp',
+    address: {
+      name: 'Jane Smith',
+      company: 'XYZ Corporation',
+      street: '456 Elm Street',
+      city: 'City2',
+      postalCode: '67890',
+      country: 'Country2'
+    },
+    productList: [
+      {
+        product: {
+          sku: 'EIQ25ZB9UQ',
+          title: 'Nut',
+          category: 'Automotive',
+          price: 75.36
+        },
+        quantity: 2
+      },
+      {
+        product: {
+          sku: '6EFD0C00DI',
+          title: 'Clamp',
+          category: 'Nautical',
+          price: 68.62
+        },
+        quantity: 3
+      },
+      {
+        product: {
+          sku: 'CMVI6FA5BC',
+          title: 'Bearing',
+          category: 'Industry',
+          price: 38.87
+        },
+        quantity: 2
+      }
+    ]
+  },
+  {
+    orderRef: 'IsLCq',
     address: {
       name: 'John Doe',
       company: 'ABC Company',
       street: '123 Main Street',
-      city: 'Example City',
+      city: 'City1',
       postalCode: '12345',
-      country: 'Country'
+      country: 'Country1'
     },
     productList: [
       {
@@ -234,24 +225,6 @@ export default defineComponent({
           price: 38.87
         },
         quantity: 3
-      },
-      {
-        product: {
-          sku: '86ZL82ERVQ',
-          title: 'Washer',
-          category: 'Industry',
-          price: 38.09
-        },
-        quantity: 4
-      },
-      {
-        product: {
-          sku: 'CMVI6FA5BC',
-          title: 'Bearing',
-          category: 'Industry',
-          price: 38.87
-        },
-        quantity: 2
       }
     ]
   }
